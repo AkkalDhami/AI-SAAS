39ed717b850808dfef9e3026a573c52e47275265
AkkalDhami <dhamiakkal21@gmail.com>
2025-08-13 11:11:54 +0545

    add image generation

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,6 +1,8 @@
 
 import OpenAI from "openai";
-import { insertArticleData } from "../services/aiServices.js";
+import { v2 as cloudinary } from "cloudinary";
+import { insertArticleData, insertImageData } from "../services/aiServices.js";
+import axios from "axios";
 
 const ai = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -62,5 +64,100 @@ export const generateArticle = async (req, res) => {
     }
 }
 
+export const generateBlogTitle = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { prompt } = req.body;
 
-export const generateImage = async (req, res) => { }
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if (plan !== 'premium' && free_usage >= 10) {
+            return res.json({
+                success: false,
+                message: "You have reached your free usage limit. Please upgrade to a premium plan to continue."
+            });
+        }
+
+        const response = await ai.chat.completions.create({
+            model: "gemini-2.5-flash",
+            reasoning_effort: "low",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: 100,
+        });
+
+        const content = response.choices[0].message.content;
+
+        await insertArticleData({ userId, content, prompt, type: 'blog-title' });
+
+        if (plan !== 'premium') {
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    free_usage: free_usage + 1
+                }
+            });
+        }
+
+        res.json({
+            success: true,
+            content
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const generateImage = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { prompt, publish } = req.body;
+
+        const plan = req.plan;
+
+        if (plan !== 'premium') {
+            return res.json({
+                success: false,
+                message: "This feature is only available to premium users."
+            });
+        }
+        const formdata = new FormData()
+        formdata.append("prompt", prompt);
+
+        const { data } = await axios.post('https://clipdrop-api.co/text-to-image/v1', formdata, {
+            headers: {
+                'x-api-key': process.env.CLIPDROP_API_KEY,
+            },
+            responseType: 'arraybuffer'
+        })
+
+        const base64Image = `data:image/png;base64,${Buffer.from(data, 'binary').toString('base64')}`
+
+        const { secure_url } = await cloudinary.uploader.upload(base64Image)
+
+        await insertImageData({ userId, secure_url, prompt, type: 'image', publish });
+
+        res.json({
+            success: true,
+            secure_url
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { generateArticle } from "../controllers/aiController.js";
+import { generateArticle, generateBlogTitle, generateImage } from "../controllers/aiController.js";
 import { auth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/generate-article", auth, generateArticle);
+router.post("/generate-blog-title", auth, generateBlogTitle);
+router.post("/generate-image", auth, generateImage);
 
 export default router;
