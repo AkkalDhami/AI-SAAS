3598e0a3b32b587ed8f646e3404898df5dba1b9b
AkkalDhami <dhamiakkal21@gmail.com>
2025-08-14 19:55:50 +0545

    add loader

---
+++ client/src/components/ui/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <span className="w-4 h-4 rounded-full border-2 border-t-transparent animate-spin"></span>
+  );
+};
+
+export default Loader;
